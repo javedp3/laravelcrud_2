e67749d1bebf0d1b50ef7a2156f9b8d23d422da6
Javed Patel <57598873+javedp3@users.noreply.github.com>
2024-08-17 17:59:05 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel jenkins</title>
+        <title>Laravel jenkins webhook</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
